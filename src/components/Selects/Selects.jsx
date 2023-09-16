c9d06c0a78f35290b66af8c129ac451c5aa36907
Jahirul83 <jahirulislamcse83@gmail.com>
2023-09-16 08:15:13 +0600

    selected courses total credit and total remaining cradit  calculated and shown in ui

--- src/components/Selects/Selects.jsx
+++ src/components/Selects/Selects.jsx
@@ -1,20 +1,40 @@
 import PropTypes from 'prop-types'
 import Selected from '../Selected/Selected'
 
-const Selects = ({ selects }) => {
+const Selects = ({ selects, remaining, totalCredit }) => {
     return (
-        <div className='w-1/2 bg-slate-200 p-4 m-4'>
-            <h3 className='text-xl font-bold mb-8'>Course Name:{selects.length}</h3>
+        <div className='w-1/2 m-2'>
+            <div className='bg-white p-2 rounded-xl'>
 
-            {
-                selects.map(selected=><Selected key={selected.id} selected={selected}></Selected>)
-            }
+                <div className='border-b-2 bg-w'>
+
+                    <h3 className='text-purple-400'>Credit Hour Remaining {remaining} hrs</h3>
+                </div>
+                <div className='border-b-2'>
+                    <h3 className='text-xl font-bold mb-8'>Course Name: {selects.length}</h3>
+
+                    {
+                        selects.map((selected, idx) => <Selected
+                            idx={idx}
+                            key={selected.id}
+                            selected={selected}
+                        ></Selected>)
+                    }
+                </div>
+                <div className='m-6'>
+                    Total credit Hour:{totalCredit}
+                </div>
+            </div>
+
         </div>
     )
 }
 
 Selects.propTypes = {
-    selects:PropTypes.object
+    selects: PropTypes.object,
+    remaining: PropTypes.number,
+    totalCredit: PropTypes.number
+
 }
 
 export default Selects
