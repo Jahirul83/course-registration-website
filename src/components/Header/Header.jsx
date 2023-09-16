0bf7e24598b2a00daf594357f271024dab409b99
Jahirul83 <jahirulislamcse83.com>
2023-09-16 23:07:47 +0600

    Total price shown in the cart section of the UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
 
     return (
         <div className='text-center'>
-            <h3 className='text-5xl'>Course Registration</h3>
+            <h3 className='text-5xl font-semibold'>Course Registration</h3>
         </div>
     );
 }
